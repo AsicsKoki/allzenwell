e974df55a4b95ba4fb8b451f168f9ef00549f91c
Konstantin <cpt.koki@gmail.com>
2018-02-09 12:14:27 +0100

    This commit will add base controllers

---
+++ app/Http/Controllers/BookingController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Session;
+use Illuminate\Support\Facades\Input;
+
+class BookingController extends Controller
+{
+	//
+}
